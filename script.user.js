aff92ff5f4efc4e1f5f1c527f0a2e7bac5c343b2
Matadon27 <mexanoid2012@gmail.com>
2023-11-12 04:37:48 +0200

    Version change

--- script.user.js
+++ script.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         OGame DataProcessing
 // @namespace    http://tampermonkey.net/
-// @version      1.25.4
+// @version      1.25.5
 // @description  Allows you to quickly and conveniently view all information about the player/alliance through the search by Name/ID/Coodinates gamer and Name/ID/Tag of the alliance respectively. Highlights activity on planets and moons
 // @author       Matadon27
 // @website      https://github.com/Matadon27
